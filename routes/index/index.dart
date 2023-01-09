59058c352d8f93ed9a3b5ba106808045edcf4a7a
Dylan J <dylan@tritan.dev>
2023-01-09 03:16:47 -0500

    added fun fact of the day 1/9/23

--- routes/index/index.dart
+++ routes/index/index.dart
@@ -1,6 +1,6 @@
 indexRoute(router){
    router.get('/', (req, res) => {
     "hey hey hey": "what's poppin what's sloppin? you in the mood for some good guaccin or are you just tryna go shoppin?", 
-    "fun fact": "did you know that cum weighs more than water!"
+    "fun facts": ["did you know that cum weighs more than water!", "cum is actually highly nutritious"]
   });
 }
