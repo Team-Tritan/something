601cef0c19b3d1a05b2bf4e23642f7de65e04e7e
Dylan J <dylan@tritan.dev>
2023-01-09 03:18:24 -0500

    one load of cum is roughly ~25 calories!

--- routes/index/index.dart
+++ routes/index/index.dart
@@ -1,6 +1,6 @@
 indexRoute(router){
    router.get('/', (req, res) => {
     "hey hey hey": "what's poppin what's sloppin? you in the mood for some good guaccin or are you just tryna go shoppin?", 
-    "fun facts": ["did you know that cum weighs more than water!", "cum is actually highly nutritious"]
+    "fun facts": ["did you know that cum weighs more than water!", "cum is actually highly nutritious", "one load of cum is roughly ~25 calories"]
   });
 }
